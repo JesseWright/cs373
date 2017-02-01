5cd8606fc2ff9e0c9b90f4108660ea5cad2f0b05
Glenn Downing <downing@cs.utexas.edu>
2017-02-01 16:35:17 -0600

    another commit

---
+++ notes/02-01.py
@@ -0,0 +1,45 @@
+# -----------
+# Wed,  1 Feb
+# -----------
+
+"""
+Bloomberg Talk
+
+frontend: JS
+backend: C++/Python
+
+Chris' Projects
+    Equity Screening
+        search engine for equities
+        JS/C++
+
+    Equity Services
+
+    Equity Application Frameworks
+        Scalable Coud Compute
+            Openstack
+        Distributed Compute Engine
+            Hadoop
+
+2003
+    Google File System
+    commodity hardware
+
+    Google Map-Reduce
+
+Apache Hadoop
+open-source
+
+Hadoop
+    Hadoop Distributed File System (HDFS)
+    Yarn resource manager
+    Map-Reduce
+
+Spark
+    next generation Map-Reduce
+    in memory compute platform
+    clean APIs in Scala, Java, Python, R
+
+Hortonworks
+Cloudera
+"""
