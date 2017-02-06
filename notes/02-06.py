96a88cc8c13ed1c4b7ebefb05769c8f4a1f55005
Glenn Downing <downing@cs.utexas.edu>
2017-02-06 14:05:28 -0600

    another commit

---
+++ notes/02-06.py
@@ -0,0 +1,13 @@
+# -----------
+# Mon,  6 Feb
+# -----------
+
+# root mean squared error
+
+a = [ 1,  2,  3,  4,  5]
+b = [ 2,  5,  3,  1,  4]
+
+d = [-1, -3,  0,  3,  1] # error
+s = [ 1,  9,  0,  9,  1] # squared
+m = 4                    # mean
+r = 2                    # root
