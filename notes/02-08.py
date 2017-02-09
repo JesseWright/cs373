9167235e4a3965c4307e4f2c3772a40cf603fe4f
Glenn Downing <downing@cs.utexas.edu>
2017-02-09 08:28:23 -0600

    another commit

---
+++ notes/02-08.py
@@ -0,0 +1,18 @@
+# -----------
+# Wed,  8 Feb
+# -----------
+
+"""
+true  division's return type is always float
+floor division's return type is its input type
+
+if tail recursion is optimized, it becomes O(1) in space
+Python does NOT optimize tail recursion
+
+in Python 2
+	range  is eager and O(n) in space
+	xrange is lazy  and O(1) in space, generator
+
+in Python3
+	range  is lazy  and O(1) in space, generator
+"""
