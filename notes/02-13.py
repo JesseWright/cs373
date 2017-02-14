b394bed6a2515b43da6c6c4d88d2921ba79fa600
Glenn Downing <downing@cs.utexas.edu>
2017-02-13 21:12:21 -0600

    another commit

--- notes/02-13.py
+++ notes/02-13.py
@@ -2,3 +2,15 @@
 # Mon, 13 Feb
 # -----------
 
+def reduce (bf, a, v) :
+    for w in a :
+        v = bf(v, w)
+    return v
+
+"""
+reduce takes a seed so that it can return that seed
+if the iterable is empty
+
+the real reduce takes a seed optionally and raises an exception
+if the iterable is empty
+"""
