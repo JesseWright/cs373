25dab54f7ece5ec3f2edab7114a2cbb8b93648cf
Glenn Downing <downing@cs.utexas.edu>
2017-03-29 17:42:37 -0500

    another commit

--- notes/03-29.py
+++ notes/03-29.py
@@ -3,4 +3,18 @@
 # -----------
 
 """
+# well-formed XML
+% xmllint --noout Bookstore1.xml
+
+# valid XML
+% xmllint --noout --valid Bookstore1.dtd.xml
+
+# DTDs can express untyped pointers with ID/IDREF
+# DTDs can express multiplicity with *, +, ?
+
+# XSDs can express typed pointers
+# XSDs can express fine-grain multiplicity
+
+# JSON can NOT express pointers
+# JSON can express fine-grain multiplicity
 """
