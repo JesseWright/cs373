9e6521afe42c0c3d9c8ed0d4226bd8dafe25837b
Glenn Downing <downing@cs.utexas.edu>
2017-03-29 10:51:51 -0500

    another commit

--- exercises/NaturalJoin.py
+++ exercises/NaturalJoin.py
@@ -10,9 +10,14 @@
 
 # http://en.wikipedia.org/wiki/Relational_algebra#.CE.B8-join_and_equijoin
 
+from typing import Dict, Iterable, Iterator
+
 from ThetaJoin import theta_join_generator
 
-def natural_join_yield (r, s) :
+def natural_join_yield (
+        r: Iterable[Dict[str, int]],
+        s: Iterable[Dict[str, int]]) \
+        -> Iterator[Dict[str, int]]  :
     def bp (u, v) :
         return all(u[k] == v[k] for k in u if k in v)
     for u in r :
@@ -20,12 +25,18 @@ def natural_join_yield (r, s) :
             if bp(u, v) :
                 yield dict(u, **v)
 
-def natural_join_generator (r, s) :
+def natural_join_generator (
+        r: Iterable[Dict[str, int]],
+        s: Iterable[Dict[str, int]]) \
+        -> Iterator[Dict[str, int]]  :
     def bp (u, v) :
         return all(u[k] == v[k] for k in u if k in v)
     return (dict(u, **v) for u in r for v in s if bp(u, v))
 
-def natural_join_theta_join (r, s) :
+def natural_join_theta_join (
+        r: Iterable[Dict[str, int]],
+        s: Iterable[Dict[str, int]]) \
+        -> Iterator[Dict[str, int]]  :
     def bp (u, v) :
         return all(u[k] == v[k] for k in u if k in v)
     return theta_join_generator(r, s, bp)
