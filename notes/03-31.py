fc8b1f3513ba92193ae023acb0eaf05e526de0c1
Glenn Downing <downing@cs.utexas.edu>
2017-03-31 16:03:35 -0500

    another commit

---
+++ notes/03-31.py
@@ -0,0 +1,27 @@
+# -----------
+# Fri, 31 Mar
+# -----------
+
+"""
+movie table
+
+name year genre director
+"shane" 1954 "western" "george stevens"
+"star wars" 1977 "western" "george lucas"
+
+transform into two tables
+
+director table
+
+id name
+1 "george stevens"
+2 "george lucas"
+
+movie table
+
+name year genre director
+"shane" 1954 "western" 1
+"star wars" 1977 "western" 2
+
+The InnoDB engine enforces foreign key validation.
+"""
