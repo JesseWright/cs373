ee7f544400f5c818f68ace7281e36c0df03f1a21
Glenn Downing <downing@cs.utexas.edu>
2017-04-17 20:30:40 -0500

    another commit

--- patterns/SingletonPatternT.java
+++ patterns/SingletonPatternT.java
@@ -3,6 +3,7 @@
 // ----------------------
 
 // https://en.wikipedia.org/wiki/Singleton_pattern
+// https://en.wikipedia.org/wiki/Initialization-on-demand_holder_idiom
 
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -17,10 +18,16 @@ public final class SingletonPatternT extends TestCase {
     	assertEquals( "Eager.f()", Eager.only().f());}
 
     public void test_3 () {
-    	assertEquals(Lazy.only(), Lazy.only());}
+    	assertEquals(Lazy1.only(), Lazy1.only());}
 
     public void test_4 () {
-    	assertEquals("Lazy.f()", Lazy.only().f());}
+    	assertEquals("Lazy1.f()", Lazy1.only().f());}
+
+    public void test_5 () {
+    	assertEquals(Lazy2.only(), Lazy2.only());}
+
+    public void test_6 () {
+    	assertEquals("Lazy2.f()", Lazy2.only().f());}
 
     public static void main (String[] args) {
         TestRunner.run(new TestSuite(SingletonPatternT.class));}}
