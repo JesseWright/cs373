2da53a5b5a6aa1ea9a005eddb35ae9b2f254ff5d
Glenn Downing <downing@cs.utexas.edu>
2017-04-22 13:01:52 -0500

    another commit

---
+++ notes/04-21.py
@@ -0,0 +1,21 @@
+# -----------
+# Fri, 21 Apr
+# -----------
+
+# return types are covariant
+
+class Game {
+    Maze createMaze () {
+        ...getRoom()...
+
+    Room getRoom () {
+        return new Room();}}
+
+class EnchantedGame extends Game {
+    EnchantedRoom getRoom () {
+        return new EnchantedRoom();}}
+
+class T {
+    public static void main (...) {
+        EnchantedGame x = new EnchantedGame();
+        EnchantedRoom y = x.getRoom();
